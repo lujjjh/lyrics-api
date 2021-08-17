23f22101f40beac774cf971da417b9f86cbb096e
Jiahao Lu <lujjjh@gmail.com>
2021-08-17 17:17:07 +0800

    Avoid exposing the original IP

--- src/index.ts
+++ src/index.ts
@@ -39,11 +39,16 @@ const handleRequest = async (event: FetchEvent) => {
   // Logging.
   {
     const status = response.status
-    const ip = request.headers.get('cf-connecting-ip')
+    const ip = request.headers.get('cf-connecting-ip') ?? ''
+    // To avoid logging the original IP, use SHA-1(ip) as user id.
+    const user_id = Array.from(new Uint8Array(await crypto.subtle.digest('SHA-1', new TextEncoder().encode(ip))))
+      .map((b) => b.toString(16).padStart(2, '0'))
+      .join('')
+      .slice(0, 5)
     event.waitUntil(
       log({
         status,
-        ip,
+        user_id,
         name,
         artist,
         hit,
